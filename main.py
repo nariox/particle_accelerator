6b27d88027066910ee428f8bf8089f5796e80fab
Doug McNanney <mcnanney@pop-os.localdomain>
2023-03-04 20:34:21 -0500

    Created basic framework for a callback when ADC pins go below a threshold, indicating that a marble has passed through. This triggers a PWM signal (corresponding coil) to pull the marble forward.

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from machine import Pin, PWM
-from time import sleep
+import machine
+import time
 
-led = PWM(Pin(23), 5000)
+# PWM signal for each coil
+pwm_pins = [23, 22, 1, 3]
+# Create PWM objects for each coil pin
+pwm_coils = [machine.PWM(machine.Pin(pin), 5000) for pin in pwm_pins]
+for coil in pwm_coils:
+    # Set frequencies to 10 Hz
+    coil.freq(10)
+    # Set the duty cycle to 0 for each PWM
+    coil.duty(0)
 
-# freq = 10 Hz
-# duty = 30
-# gives about 200mA
+# Sensor pins which corelate to the coil pins
+sensor_pins = [36, 39, 34, 35]
+# Define the IR sensor thresholds, signal goes HI to LO when marble is detected
+thresholds = [512, 512, 512, 512]  # adjust these values for your sensors
 
-led.freq(10)
-led.duty(30)
+
+# Turn on the PWM for a given index, for 0.1 seconds
+# TODO: Make this a non-blocking function
+def pulse_coil(idx):
+    pwm_coils[idx].duty(30)   # set duty cycle to ~3%
+    time.sleep(0.1)      # wait for 0.1 seconds
+    pwm_coils[idx].duty(0)    # turn off the PWM
+
+# Define the function to handle an analog sensor reading
+def on_sensor_change(pin):
+    idx = sensor_pins.index(pin)  # get index of ir sensor pin
+    val = machine.ADC(pin).read() # read analog value of ir sensor
+    if val < thresholds[idx]:     # check if the value is below the threshold
+        pulse_coil(idx)           # activate the corresponding PWM signal
+
+# Configure the sensor pins as analog inputs
+for pin in sensor_pins:
+    p = machine.ADC(machine.Pin(pin))
+    p.atten(machine.ADC.ATTN_11DB)   # set the attenuation to 11 dB
+    p.width(machine.ADC.WIDTH_12BIT) # set the width to 12 bits
+
+# Configure the sensor pins to trigger interrupts on value change
+for pin in sensor_pins:
+    p = machine.ADC(machine.Pin(pin))
+    p.irq(trigger=machine.Pin.IRQ_ANYEDGE, handler=on_sensor_change)
