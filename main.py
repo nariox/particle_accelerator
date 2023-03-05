d7fe1932155f9ab5f7d25a3a96ba886dff868e4b
Douglas McNanney <d.mcnanney@gmail.com>
2023-03-05 10:48:42 -0500

    Tweaked for a single coil - sensor test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import machine
 import time
 
 # PWM signal for each coil
-pwm_pins = [23, 22, 1, 3]
+pwm_pins = [23] #, 22, 1, 3]
 # Create PWM objects for each coil pin
 pwm_coils = [machine.PWM(machine.Pin(pin), 5000) for pin in pwm_pins]
 for coil in pwm_coils:
@@ -12,9 +12,9 @@ for coil in pwm_coils:
     coil.duty(0)
 
 # Sensor pins which corelate to the coil pins
-sensor_pins = [36, 39, 34, 35]
+sensor_pins = [36] #, 39, 34, 35]
 # Define the IR sensor thresholds, signal goes HI to LO when marble is detected
-thresholds = [512, 512, 512, 512]  # adjust these values for your sensors
+thresholds = [512] #, 512, 512, 512]  # adjust these values for your sensors
 
 
 # Turn on the PWM for a given index, for 0.1 seconds
