7926750e1461a9a1ae4b86f228ec1386f13052cf
Doug McNanney <mcnanney@frib.msu.edu>
2023-03-14 17:15:28 -0400

    Implimentation of pwm test for four coils

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
 import machine
 import time
 
-print("Starting Output")
+# Set up the ADC pins (Phototransistor) and output pins (Coils)
+ADC_PINS = [machine.Pin(36), machine.Pin(39), machine.Pin(34), machine.Pin(35)]
+OUT_PINS = [machine.Pin(23, machine.Pin.OUT), machine.Pin(22, machine.Pin.OUT), machine.Pin(1, machine.Pin.OUT), machine.Pin(3, machine.Pin.OUT)]
 
-# PWM signal for each coil
-pwm_pins = [23] #, 22, 1, 3]
-# Create PWM objects for each coil pin
-pwm_coils = [machine.PWM(machine.Pin(pin), 5000) for pin in pwm_pins]
-for coil in pwm_coils:
-    # Set frequencies to 10 Hz
-    coil.freq(10)
-    # Set the duty cycle to 0 for each PWM
-    coil.duty(0)
+# Define constants
+# Define the threshold values for the ADCs
+ADC_THRESHOLDS = [900, 900, 900, 900]
+# Define the number of consecutive samples below the threshold needed to trigger the output pulse
+CONSECUTIVE_SAMPLES = 5
+# Define the duration of the output pulse in milliseconds
+PULSE_DURATION = 10 # ms
+# Configure minimum time between pulses in milliseconds
+MAX_PULSE_INTERVAL = 10 # ms
+# PWM frequency
+PWM_FREQUENCY = 10 # Hz
 
-# Sensor pins which corelate to the coil pins
-sensor_pins = [36] #, 39, 34, 35]
-# Define the IR sensor thresholds, signal goes HI to LO when marble is detected
-thresholds = [512] #, 512, 512, 512]  # adjust these values for your sensors
+# Initialize variables
+# Number of consecutive samples below the threshold for each input
+consecutive_below_thresholds = [0] * len(ADC_PINS)
+# Time since the last output pulse for each output
+last_pulse_times = [time.ticks_ms()] * len(OUT_PINS)
+# Create hardware timer for each output
+timers = [machine.Timer(i) for i in range(len(OUT_PINS))]
+# Create an instance of the PWM object for each output
+pwms = [machine.PWM(pin, freq=PWM_FREQUENCY, duty=0) for pin in OUT_PINS]
 
 
-# Turn on the PWM for a given index, for 0.1 seconds
-# TODO: Make this a non-blocking function
-def pulse_coil(idx):
-    print("Pulsing!")
-    pwm_coils[idx].duty(30)   # set duty cycle to ~3%
-    time.sleep(0.1)      # wait for 0.1 seconds
-    pwm_coils[idx].duty(0)    # turn off the PWM
+# To be called when the timer triggers
+def timer_callback(timer):
+    pwm_index = OUT_PINS.index(timer.arg)
+    pwms[pwm_index].duty(0)
 
-# Define the function to handle an analog sensor reading
-def on_sensor_change(pin):
-    idx = sensor_pins.index(pin)  # get index of ir sensor pin
-    val = machine.ADC(pin).read() # read analog value of ir sensor
-    print("Value", val)
-    if val < thresholds[idx]:     # check if the value is below the threshold
-        pulse_coil(idx)           # activate the corresponding PWM signal
 
-# Configure the sensor pins as analog inputs
-for pin in sensor_pins:
-    p = machine.Pin(pin, machine.Pin.IN)
-    adc = machine.ADC(p)
-    adc.atten(machine.ADC.ATTN_11DB)   # set the attenuation to 11 dB
-    adc.width(machine.ADC.WIDTH_12BIT) # set the width to 12 bits
+while True:
+    for i in range(len(ADC_PINS)):
+        # Read the ADC value
+        adc_value = ADC_PINS[i].read()
 
-# Configure the sensor pins to trigger interrupts on value change
-for pin in sensor_pins:
-    p = machine.Pin(pin, machine.Pin.IN)
-    p.irq(trigger=machine.Pin.IRQ_FALLING, handler=on_sensor_change)
+        # Check if the ADC value is below the threshold
+        if adc_value < ADC_THRESHOLDS[i]:
+            consecutive_below_thresholds[i] += 1
+        else:
+            consecutive_below_thresholds[i] = 0
 
-# For serial Debugging:
-uart = machine.UART(0, 115200)
+        # Check if we have enough consecutive samples below the threshold to trigger the output pulse
+        if consecutive_below_thresholds[i] >= CONSECUTIVE_SAMPLES:
+            # Calculate the elapsed time since the last pulse for this output
+            elapsed_time = time.ticks_diff(time.ticks_ms(), last_pulse_times[i])
+
+            # Check if we have waited long enough to trigger another pulse for this output
+            if elapsed_time >= MAX_PULSE_INTERVAL:
+                # Turn on the output pin PWM signal for this output
+                pwms[i].duty(5)
+            
+                # Set the timer to trigger after the pulse duration and call the timer_callback function
+                # This will turn off the PWM signal on this coil after PULSE_DURATION ms
+                timers[i].init(period=PULSE_DURATION, mode=machine.Timer.ONE_SHOT, arg=OUT_PINS[i], callback=timer_callback)
+
+                # Reset the consecutive samples counter for this input and update the last pulse time for this output
+                consecutive_below_thresholds[i] = 0
+                last_pulse_times[i] = time.ticks_ms()
