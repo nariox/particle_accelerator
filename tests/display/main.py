0455c9b2a95576326ce213aad1eed47ece784879
Douglas McNanney <d.mcnanney@gmail.com>
2023-04-16 12:40:36 -0500

    Test script for OLED display

---
+++ tests/display/main.py
@@ -0,0 +1,8 @@
+from machine import Pin, I2C
+import ssd1306
+
+# using default address 0x3C
+i2c = I2C(1, scl=Pin(22), sda=Pin(21))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+display.text('Hello, World!', 0, 0, 1)
+display.show()
